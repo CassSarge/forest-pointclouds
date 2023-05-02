67b189b03004f3e9a5266b267999236c120c7a03
Cassandra <cassandrasargeant1@gmail.com>
2023-05-02 11:19:36 +1000

    Change to tensorflow from pytorch

--- annotationOpen.py
+++ annotationOpen.py
@@ -1,23 +1,40 @@
 import os
 import pickle
 import numpy as np
-import torch
-import torch.utils as utils
 
-with open("plot_annotations.p", "rb") as f:
+import tensorflow as tf
+
+with open("data/plot_annotations.p", "rb") as f:
     annotations = pickle.load(f)
 data = np.asarray(annotations)
 print(data.shape) # (10829404, 4)
 print(type(data))
 features = data[:, 0:3] # Features between 
-print(features.shape)
 labels = data[:, 3] # Label between 0 and 3 (4 classes)
+print(features.shape)
 print(labels.shape)
-# print(labels[0:100])
 print(features[1])
+print(labels[1:50])
 
-train_x = torch.Tensor(features)
-train_y = torch.Tensor(labels)
+print(type(features))
+print(type(features[1]))
+print(type(features[1][1]))
 
-dataset = utils.data.TensorDataset(train_x, train_y)
-train_loader = utils.data.DataLoader(dataset, batch_size=100, shuffle=True)
+print(type(labels))
+print(type(labels[1]))
+
+
+# print("Creating TFRecord file...")
+# with tf.io.TFRecordWriter("plot_annotations.tfrecord") as writer:
+#     # Make TFRecord file from numpy array
+#     for i in range(len(features)):
+
+#         # Create a feature
+#         feature = {
+#             'points': tf.train.Feature(float_list=tf.train.FloatList(value=features[i])),
+#             'labels': tf.train.Feature(int64_list=tf.train.Int64List(value=[labels[i]]))
+#         }
+#         # Create an example protocol buffer
+#         example = tf.train.Example(features=tf.train.Features(feature=feature))
+#         # Serialize to string and write on the file
+#         writer.write(example.SerializeToString())
