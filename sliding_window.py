c504babfa46e72318e3cd3ea7a1b56adb04db6a5
NickSarge <nicholassargeant@gmail.com>
2023-08-22 16:57:15 +1000

    Subsample and save to tfrecord

--- sliding_window.py
+++ sliding_window.py
@@ -1,9 +1,21 @@
 import numpy as np
 import pickle
+import tensorflow as tf
 
-from generateTFRecord import load_dataset
+from generateTFRecord import load_dataset, basic_example_from_data
 
-def sliding_window(features, labels, window_width: int, stride: int, sample_size: int = 8192):
+def subsample_to_TFrecord(writer, data, sample_size, n_subsamples, randomizer):
+
+    # Repeatedly subsample the data and save it to the TFRecord file
+    for i in range(n_subsamples):
+        subsampled_data = randomizer.choice(data, sample_size, replace=True)
+        current_example = basic_example_from_data(subsampled_data[:, 0:3], subsampled_data[:, 3])
+        writer.write(current_example.SerializeToString())
+
+
+    return
+
+def sliding_window(full_data, window_width: int, stride: int, sample_size: int = 8192):
 
     start_x = -19
     start_y = -19
@@ -16,27 +28,40 @@ def sliding_window(features, labels, window_width: int, stride: int, sample_size
 
     i = 0
 
-    while current_y < end_y:
+    rng = np.random.default_rng(12345)
+    with tf.io.TFRecordWriter("data/full_w_subsampling.tfrecord") as writer:
 
-        while current_x < end_x:
-            # Get points within window
-            current_features = features[(features[:,0] >= current_x - window_width) & (features[:,0] < current_x + window_width) & (features[:,1] >= current_y - window_width) & (features[:,1] < current_y + window_width)]
-            # Get labels within window
-            # current_labels = labels[(features[:,0] >= current_x - window_width) & (features[:,0] < current_x + window_width) & (features[:,1] >= current_y - window_width) & (features[:,1] < current_y + window_width)]
-            # Count number of points and print
-            (num_points, _) = current_features.shape
-            # (num_labels) = current_labels.shape
-            # Point densities range from about 0 to 40000 within a window right now
-            # This loop might suck investigate sorting methods?
-            print("i: {}, num_points: {}, Current x: {}, Current y: {}".format(i, num_points, current_x, current_y))
-            i += 1
+        while current_y < end_y:
 
-            current_x += stride
+            while current_x < end_x:
+                # Get points within window
+                current_data = full_data[(full_data[:,0] >= current_x - window_width) & (full_data[:,0] < current_x + window_width) & (full_data[:,1] >= current_y - window_width) & (full_data[:,1] < current_y + window_width)]
+                # Get labels within window
+                # Count number of points and print
+                (num_points, _) = current_data.shape
+                # (num_labels) = current_labels.shape
+                # Point densities range from about 0 to 40000 within a window right now
+                # Choose a random subsample of these
+                if num_points > sample_size:
+                    subsample_to_TFrecord(writer, current_data, sample_size, 30, rng)
+                # Else if there are less than 8192 points, but more than 4096, we'll upsample
+                elif num_points > sample_size / 2:
+                    # Only sample a bit if theres a low number of points
+                    subsample_to_TFrecord(writer, current_data, sample_size, 5, rng)
+                else:
+                    pass
+                
+                print("i: {}, num_points: {}, Current x: {}, Current y: {}".format(i, num_points, current_x, current_y))
 
-        current_y += stride
-        current_x = start_x
+                
+                i += 1
 
-    print("Done!")
+                current_x += stride
+
+            current_y += stride
+            current_x = start_x
+
+        print("Done!")
     return
 
 
@@ -44,15 +69,7 @@ def sliding_window(features, labels, window_width: int, stride: int, sample_size
 if __name__ == '__main__':
     # Min data = -20,-20, 0
     # Max data = 20, 20, ~55
-    features, labels = load_dataset()
-    print(features.shape)
-
-    sliding_window(features, labels, 1, 0.5)
-
-    # print(np.amin(features[:,0]))
-    # print(np.amax(features[:,0]))
-    # print(np.amin(features[:,1]))
-    # print(np.amax(features[:,1]))
-    # print(np.amin(features[:,2]))
-    # print(np.amax(features[:,2]))
+    full_data = load_dataset(False)
+  
+    sliding_window(full_data, 1, 0.5)
 
