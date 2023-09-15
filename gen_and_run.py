aaae7fff6255b45da98db12956869fccfd61c19b
Cassandra <cassandrasargeant1@gmail.com>
2023-09-15 16:07:41 +1000

    Single file for generating data and training

---
+++ gen_and_run.py
@@ -0,0 +1,12 @@
+from basic_treedata import train, setup
+from sliding_window import create_records_using_window
+
+if __name__ == '__main__':
+
+    window_width = int(input("Enter window width: "))
+
+    create_records_using_window(window_width)
+    
+    config = setup(window_width)
+    
+    train(config)
