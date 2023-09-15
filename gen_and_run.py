39a923c87cdbc790e61b02ef6c8992b141d4cddd
Cassandra <cassandrasargeant1@gmail.com>
2023-09-15 16:09:28 +1000

    Accept floats as inputs

--- gen_and_run.py
+++ gen_and_run.py
@@ -3,10 +3,10 @@ from sliding_window import create_records_using_window
 
 if __name__ == '__main__':
 
-    window_width = int(input("Enter window width: "))
+    window_width = float(input("Enter window width: "))
 
     create_records_using_window(window_width)
     
     config = setup(window_width)
-    
+
     train(config)
