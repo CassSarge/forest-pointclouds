d06876c15f56255bcff4196df1812599304fe1dd
Cassandra <cassandrasargeant1@gmail.com>
2023-10-16 19:24:47 +1100

    More graphs

--- history_graphs.py
+++ history_graphs.py
@@ -1,22 +1,31 @@
 import matplotlib.pyplot as plt
 import pickle
 
-def plot_result(history, item):
+def plot_result(history, item, window_width: None):
     try: history = history.history
     except: pass
+
+    print(window_width)
+
+    if window_width is not None:
+        title = "Train and Validation {} Over Epochs with Window Width {}".format(item, window_width)
+    else:
+        title = "Train and Validation {} Over Epochs".format(item)
 
     plt.plot(history[item], label=item)
     plt.plot(history["val_" + item], label="val_" + item)
     plt.xlabel("Epochs")
     plt.ylabel(item)
-    plt.title("Train and Validation {} Over Epochs".format(item), fontsize=14)
+    plt.title(title, fontsize=14)
     plt.legend()
     plt.grid()
+    plt.ylim(0, 1)
     plt.show()
+
 
 if __name__ == '__main__':
     # List all log directories to use
-    nums = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    nums = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]
     dirs = ["trees_{}".format(num) for num in nums]
 
     # List all attributes for each history
@@ -63,6 +72,58 @@ if __name__ == '__main__':
     plt.xlabel("Window Width")
     plt.ylabel("Cross-Entropy")
     plt.title("Final Loss Values Across Experiments", fontsize=14)
+    plt.legend()
+    plt.grid()
+    plt.ylim(0, 1)
+    plt.show()
+
+    # # Generate plots for each log showing how each attribute changed over the 50 epochs, including window width
+    # for dir in dirs:
+    #     with open('./logs/{}/trainHistoryDict'.format(dir), 'rb') as f:
+    #         history = pickle.load(f)
+    #         # plot_result(history, "loss", window_width=dir[-3:])
+    #         # plot_result(history, "sparse_cat_acc", window_width=dir[-3:])
+    #         plot_result(history, "meanIoU", window_width=dir[-3:])
+    #         # plot_result(history, "FoliageIoU", window_width=dir[-3:])
+    #         # plot_result(history, "StemIoU", window_width=dir[-3:])
+    #         # plot_result(history, "GroundIoU", window_width=dir[-3:])
+    #         # plot_result(history, "UndergrowthIoU", window_width=dir[-3:])
+
+    # Plot all the meanIoUs together on one graph across all epochs without validation data
+    for dir in dirs:
+        with open('./logs/{}/trainHistoryDict'.format(dir), 'rb') as f:
+            history = pickle.load(f)
+            plt.plot(history["meanIoU"], label=dir)
+
+    plt.xlabel("Epochs")
+    plt.ylabel("IoU")
+    plt.title("Mean IoU Across Epochs", fontsize=14)
+    plt.legend()
+    plt.grid()
+    plt.ylim(0, 1)
+    plt.show()
+
+    for dir in dirs:
+        with open('./logs/{}/trainHistoryDict'.format(dir), 'rb') as f:
+            history = pickle.load(f)
+            plt.plot(history["loss"], label=dir)
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Cross-Entropy")
+    plt.title("Loss Across Epochs", fontsize=14)
+    plt.legend()
+    plt.grid()
+    plt.ylim(0, 1)
+    plt.show()
+
+    for dir in dirs:
+        with open('./logs/{}/trainHistoryDict'.format(dir), 'rb') as f:
+            history = pickle.load(f)
+            plt.plot(history["sparse_cat_acc"], label=dir)
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy Across Epochs", fontsize=14)
     plt.legend()
     plt.grid()
     plt.ylim(0, 1)
